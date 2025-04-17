8469dd3a5fe9fa3aed05cfbd5d47d5a928a9cbea
amansigh8052 <singhaman8796@gmail.com>
2025-04-17 12:08:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//header section
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  document.querySelectorAll("section").forEach((section) => {
+    const sectionTop = section.offsetTop - 60;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+const typed = new Typed('#greeting', {
+  strings: ["Hello!"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: false
+});
+const type = new Typed('#servicesList', {
+  strings: ["Web Developer","Software Developer","UI/UX Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true
+});
